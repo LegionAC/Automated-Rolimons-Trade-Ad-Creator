03f1f83cd37df38c415a688eb766bd2ce63f836c
LegionAC <battletowere9@gmail.com>
2026-07-05 17:39:42 +0100

    Fix error handling for trade response status

--- main.cpp
+++ main.cpp
@@ -105,7 +105,7 @@ int startLoop() {
         } else {
             std::cout << "Trade error: " << res->status << "\nError code: " << res->body << "\n";
             
-            if (res->status == 400) {
+            if (res->status == 400 || 401) {
                 startLoop();
             }
 
